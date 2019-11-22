c65b46a136bfb7b5d1f5043c95d0018607995dff
Glenda Gonzalez <ggonz245@fiu.edu>
2019-11-21 21:17:23 -0500

    sign up

--- src/components/Navbars/AdminNavbarLinks.jsx
+++ src/components/Navbars/AdminNavbarLinks.jsx
@@ -22,60 +22,48 @@ import UserProfile from "views/UserProfile";
 
 class AdminNavbarLinks extends Component {
   render() {
-    const notification = (
-      <div>
-        <i className="fa fa-globe" />
-        <b className="caret" />
-        <span className="notification">5</span>
-        <p className="hidden-lg hidden-md">Notification</p>
-      </div>
-    );
+    
     return (
       <div>
         <Nav>
           <NavItem eventKey={1} href="#">
-            <i className="fa fa-dashboard" />
             <p className="hidden-lg hidden-md">Dashboard</p>
           </NavItem>
-          <NavDropdown
-            eventKey={2}
-            title={notification}
-            noCaret
-            id="basic-nav-dropdown"
-          >
-            <MenuItem eventKey={2.1}>Notification 1</MenuItem>
-            <MenuItem eventKey={2.2}>Notification 2</MenuItem>
-            <MenuItem eventKey={2.3}>Notification 3</MenuItem>
-            <MenuItem eventKey={2.4}>Notification 4</MenuItem>
-            <MenuItem eventKey={2.5}>Another notifications</MenuItem>
-          </NavDropdown>
-          <NavItem eventKey={3} href="#">
-            <i className="fa fa-search" />
-            <p className="hidden-lg hidden-md">Search</p>
-          </NavItem>
-
-          
-
         </Nav>
+        
         <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Account
-          </NavItem>
+
           <NavDropdown
-            eventKey={2}
             title="Login"
             id="basic-nav-dropdown"
           >
-          <input type="text"  class="form-control" placeholder="Username" required="required" ></input>
+          
+            <input type="text"  class="form-control" placeholder="Username" required="required" ></input>
             <input type="password" class="form-control" placeholder="Password" required="required"></input>
             <input type="submit" class="btn btn-primary btn-block" value="Login" ></input>
             <div class="form-footer">
-            <a href="./table">Forgot Your password?</a>
+              <a href="#">Forgot Your password?</a>
             </div>
           </NavDropdown>
-          <NavItem eventKey={3} href="#">
-            Log out
-          </NavItem>
+
+          <NavDropdown
+            title="Sign up"
+            id="basic-nav-dropdown"
+          >
+            <p class="hint-text"> Fill in this form to create your account!</p>
+            <div class = "form-group">
+              <input type="text"  class="form-control" placeholder="Username" required="required" ></input>
+            </div>
+            <div class = "form-group">
+              <input type="password" class="form-control" placeholder="Password" required="required"></input>
+            </div><div class = "form-group">
+              <input type="password" class="form-control" placeholder="Confirm Password" required="required"></input>
+            </div>
+        
+            <input type="submit" class="btn btn-primary btn-block" value="Sign up" ></input>
+
+          </NavDropdown>
+
         </Nav>
       </div>
     );
@@ -83,3 +71,4 @@ class AdminNavbarLinks extends Component {
 }
 
 export default AdminNavbarLinks;
+
